a055ae3c3ae12ed87db2b432c5f809d12acbfaa5
paragonsean <cos30degrees@gmail.com>
2024-06-12 11:54:23 -0300

    [CHANGE] Change version

--- twitter/__version__.py
+++ twitter/__version__.py
@@ -1,5 +1,5 @@
 __title__ = "twitter-api-client"
 __description__ = "Implementation of Twitter's v1, v2, and GraphQL APIs."
-__version__ = "1.0.2"
+__version__ = "1.0.6"
 __author__ = "Vox Radar"
 __license__ = "MIT"
